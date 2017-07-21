ac10271d56affff41b6a976f4799988e0405c0d2
ga83fiz <ga83fiz@mytum.de>
2017-07-21 16:38:34 +0200

    Move models to a different file

---
+++ models.py
@@ -0,0 +1,229 @@
+'''
+Created on Jul 21, 2017
+
+@author: yawarnihal
+'''
+
+arch0 = {
+    'use_inverse_layers': False,
+    'name': 'fc-500_fc-500_fc-2000_fc-10',
+    'layers_encode': [
+        {
+            'layer_type':'Input',
+            'output_shape': [1, 1, 28 * 28]
+        },
+        {
+            'layer_type':'Dense',
+            'num_units': 500,
+        },
+        {
+            'layer_type':'Dense',
+            'num_units': 500,
+        },
+        {
+            'layer_type':'Dense',
+            'num_units': 2000,
+        },
+        {
+            'layer_type':'Encode',
+            'num_units': 10,
+            'non_linearity': 'linear'
+        },
+    ]
+} 
+
+arch1 = {
+'use_inverse_layers': True,
+'name': 'c-6-5_p_c-6-5_p',
+'layers_encode': [
+      {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+      {
+        'layer_type':'Conv2D',
+        'num_filters': 6,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+       {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+       {
+        'layer_type':'Conv2D',
+        'num_filters': 6,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+       {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+    ]
+}
+
+arch2 = {
+'use_inverse_layers': True,
+'name':'c-5-32_p_c-5-32_p',
+'layers_encode': [
+      {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+      {
+        'layer_type':'Conv2D',
+        'num_filters': 32,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+       {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+       {
+        'layer_type':'Conv2D',
+        'num_filters': 32,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+       {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+    ]
+}
+
+arch3 = {
+'use_inverse_layers': True,
+'name':'c-6-5_p',
+'layers_encode': [
+      {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+      {
+        'layer_type':'Conv2D',
+        'num_filters': 6,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+       {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+    ]
+}
+
+
+arch4 = {
+# Half of arch 2
+'use_inverse_layers': True,
+'name': 'c-5-32_p',
+'layers_encode': [
+      {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+      {
+        'layer_type':'Conv2D',
+        'num_filters': 32,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+       {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+    ]
+}
+
+arch5 = {
+# Arch 4 with Fully connected encode layer
+# Train accuracy = 10, irrespective of number of layers
+'use_inverse_layers': True,
+'name': 'c-5-32_p_fc-10',
+'layers_encode': [
+        {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+        {
+        'layer_type':'Conv2D',
+        'num_filters': 32,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+        {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+        {
+        'layer_type':'Encode',
+        'encode_size':10,
+        'non_linearity': 'linear',
+        },
+    ]
+}
+
+arch6 = {
+'use_inverse_layers': True,
+'name': 'c-5-64_p_fc-10',
+'layers_encode': [
+        {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+        {
+        'layer_type':'Conv2D',
+        'num_filters': 64,
+        'filter_size': (5, 5),
+        'non_linearity': 'rectify'
+        },
+        {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+        {
+        'layer_type':'Encode',
+        'encode_size':10,
+        'non_linearity': 'linear'
+        },
+    ]
+}
+
+arch7 = {
+'use_inverse_layers': False,
+'name': 'c-3-32_p_c-3-64_p_fc-32',
+'layers_encode': [
+        {
+        'layer_type':'Input',
+        'output_shape': [1, 28, 28]
+        },
+        {
+        'layer_type':'Conv2D',
+        'num_filters': 32,
+        'filter_size': (3, 3),
+        'non_linearity': 'rectify'
+        },
+        {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+        {
+        'layer_type':'Conv2D',
+        'num_filters': 64,
+        'filter_size': (3, 3),
+        'non_linearity': 'rectify'
+        },
+        {
+        'layer_type':'MaxPool2D',
+        'filter_size': (2, 2),
+        },
+        {
+        'layer_type':'Encode',
+        'encode_size':32,
+        'non_linearity': 'rectify'
+        },
+    ]
+}
